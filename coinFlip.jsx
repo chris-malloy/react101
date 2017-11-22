3cca3a88668557f741737c2a2251ea4585bf1a39
chris-malloy <chrismalloymusic@gmail.com>
2017-11-22 10:28:49 -0500

    added helper method

--- coinFlip.jsx
+++ coinFlip.jsx
@@ -1,4 +1,24 @@
+// pure component ... defeats the purpose of react
+// function CoinFlip(props){
+//     return(
+//         <h1>{props.title}</h1>
+//     )
+// }
+
+// stateful component
+// start with helper method
+// it allows you to create a class in ES5
+// it takes an object.  objects have properties.
+// the only rule for these properties --> must have a render function
+var CoinFlip = React.createClass({
+    render: function(){
+        return(
+            <h1>Sanity Check</h1>
+        );
+    },
+});
+
 ReactDOM.render(
-    <h1>Sanity Check</h1>,
+    <CoinFlip title="Flip the Coin" />,
     document.getElementById('root')
 )
